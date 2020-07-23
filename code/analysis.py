51c20c5e026514ae31dd0e1cf18d6798ecaab6cb
nico <nescoba@iu.edu>
2020-07-23 12:55:48 -0400

    0723_1255

--- code/analysis.py
+++ code/analysis.py
@@ -5,7 +5,7 @@ import random
 import numpy as np
 import matplotlib.pyplot as plt
 
-M = 2
+M = 10
 n = 100
 TIME = 2000
 
@@ -22,10 +22,10 @@ for m in range(M):
 
     initial_groups.append(group)
 
-world = World(initial_groups, eta = 0.006, mu = 0.006, B = 100)
+world = World(initial_groups, eta = 0.006, mu = 0.006, B = 10)
 
-avg_contr_series = []
-group_population_series = []
+contr_series = []
+#group_population_series = []
 time_series = []
 
 time = 0
@@ -36,35 +36,39 @@ while time < TIME:
     num_of_transitions += 1
     print(time)
 
-    avg_contribution = 0
+    contributions = 0
+#
+    # k = 0
+    # selected_group = world.groups[k]
+    # while selected_group.id != 0:
+        # k += 1
+        # selected_group = world.groups[k]
+#
+    # if selected_group.size != 0:
+#
 
-    k = 0
-    selected_group = world.groups[k]
-    while selected_group.id != 0:
-        k += 1
-        selected_group = world.groups[k]
-
-    if selected_group.size != 0:
-        avg_contribution = selected_group.num_of_coops/selected_group.size
-    avg_contr_series.append(avg_contribution)
-    group_population_series.append(selected_group.size)
+    total_contributions = sum([group.num_of_coops for group in world.groups])
+    #avg_contribution = selected_group.num_of_coops/selected_group.size
+    contr_series.append(total_contributions)
+    #group_population_series.append(selected_group.size)
     time_series.append(time)
 
-avg_contr_series_array = np.array(avg_contr_series)
-group_population_series_array = np.array(group_population_series)
+contr_series_array = np.array(contr_series)
+#group_population_series_array = np.array(group_population_series)
 time_series_array = np.array(time_series)
 
 fig, ax1 = plt.subplots()
-ax1.plot(time_series_array, avg_contr_series_array, label = 'Average contribution in group')
+ax1.plot(time_series_array, contr_series_array, label = 'Average contribution in group')
 
 ax1.set_xlabel('time')
-ax1.set_ylabel('Average level of cooperation')
-ax1.set_title('Analogue of Figure 1')
+ax1.set_ylabel('Total number of cooperators')
+#ax1.set_title('Analogue of Figure 1')
 #ax1.legend()
 
-ax2 = ax1.twinx()
-ax2.plot(time_series_array, group_population_series_array, label = 'Group size', color = 'r')
-ax2.set_ylabel('group size')
-ax2.legend()
-
+#
+# ax2 = ax1.twinx()
+# ax2.plot(time_series_array, group_population_series_array, label = 'Group size', color = 'r')
+# ax2.set_ylabel('group size')
+# ax2.legend()
+#
 plt.show()
